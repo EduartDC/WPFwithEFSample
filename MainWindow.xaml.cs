b1b2c663fa9de1039c9d32a4a21175bef805c2b2
Eduar <Eduar@DESKTOP-F2DQ5DU>
2022-09-20 00:20:52 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFwithEFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ProductContext _context = new ProductContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            
+            System.Windows.Data.CollectionViewSource categoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("categoryViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // categoryViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource categoryViewSource1 = ((System.Windows.Data.CollectionViewSource)(this.FindResource("categoryViewSource1")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // categoryViewSource1.Source = [generic data source]
+
+            _context.Categories.Load();
+
+            // After the data is loaded call the DbSet<T>.Local property
+            // to use the DbSet<T> as a binding source.
+            categoryViewSource.Source = _context.Categories.Local;
+            categoryViewSource1.Source = _context.Categories.Local;
+        }
+
+        private void categoryDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var product in _context.Products.Local.ToList())
+            {
+                if (product.Category == null)
+                {
+                    _context.Products.Remove(product);
+                }
+            }
+
+            _context.SaveChanges();
+            // Refresh the grids so the database generated values show up.
+            this.categoryDataGrid.Items.Refresh();
+            this.productsDataGrid.Items.Refresh();
+        }
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this._context.Dispose();
+        }
+
+       
+    }
+}
